3b66701cd716d2a34143b42465d664603b6f01f7
safawat19 <safawat.hossain@byslglobal.com>
2023-02-28 17:05:26 +0600

    user register done

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
 
 
 //configure env
@@ -16,6 +17,9 @@ const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 
+
+app.use("/api/v1/auth", authRoutes);
+
 //rest api
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to ecommerce app</h1>");
@@ -26,8 +30,5 @@ const PORT = process.env.PORT || 8080;
 
 //run listen
 app.listen(PORT, () => {
-  console.log(
-    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
-      .white
-  );
+  console.log( `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white);
 });
