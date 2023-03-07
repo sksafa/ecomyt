6c4305350a36df5b4953c8bc1f60a0d07982ef70
safawat hossain <sifakatali@gmail.com>
2023-03-08 00:22:42 +0600

    product details page done

--- client/src/pages/ProductDetails.js
+++ client/src/pages/ProductDetails.js
@@ -3,10 +3,14 @@ import React, { useState, useEffect } from "react";
 import Layout from "./../components/Layout/Layout";
 import axios from "axios";
 import { useParams, useNavigate } from "react-router-dom";
+import './HomeStyle.css'
+import toast from "react-hot-toast";
+import { useCart } from "../context/cart";
 const ProductDetails = () => {
   const params = useParams();
   const navigate = useNavigate();
   const [product, setProduct] = useState({});
+  const [cart, setCart] = useCart();
   const [relatedProducts, setRelatedProducts] = useState([]);
 
   //initalp details
@@ -36,57 +40,88 @@ const ProductDetails = () => {
       console.log(error);
     }
   };
+
+
+  const setCartItem = (p) => {
+    if (cart.some(item => item._id === p._id)) {
+      toast.error("You have already added this Item");
+      return;
+    } else {
+      setCart([...cart, p]);
+      localStorage.setItem(
+        "cart",
+        JSON.stringify([...cart, p])
+      );
+      toast.success("Item Added to cart");
+    }
+  }
+
   return (
     <Layout>
-      <div className="row container mt-2">
-        <div className="col-md-6">
-          <img
-            src={`/api/v1/product/product-photo/${product._id}`}
-            className="card-img-top"
-            alt={product.name}
-            height="300"
-            width={"350px"}
-          />
+      <div className="container productDetails">
+        <div className="row">
+          <div className="col-md-6">
+            <img
+              src={`/api/v1/product/product-photo/${product._id}`}
+              className="card-img-top"
+              alt={product.name}
+              height="500"
+              width={"300px"}
+            />
+          </div>
+          <div className="col-md-6 ">
+            <h2 className="">Product <span className="text-dark">Details</span></h2>
+            <h4>Name : {product.name}</h4>
+            <h6>Description : {product.description}</h6>
+            <h6 className="fw-bold">Price : ${product.price}</h6>
+            <h6>Category : {product?.category?.name}</h6>
+            <button
+              className="button-2 ms-1"
+              onClick={() => setCartItem(product)}
+            >
+              Add To Cart
+            </button>
+          </div>
         </div>
-        <div className="col-md-6 ">
-          <h1 className="text-center">Product Details</h1>
-          <h6>Name : {product.name}</h6>
-          <h6>Description : {product.description}</h6>
-          <h6>Price : {product.price}</h6>
-          <h6>Category : {product?.category?.name}</h6>
-          <button class="btn btn-secondary ms-1">ADD TO CART</button>
+
+        <hr />
+        <div className="row container">
+          <h4>Similar Products</h4>
+          {relatedProducts.length < 1 && (
+            <p className="text-center">No Similar Products found</p>
+          )}
+          <div className="d-flex flex-wrap">
+            {relatedProducts?.map((p) => (
+                <div className="card m-3 mb-5" style={{ width: "18rem" }} key={p._id}>
+                  <img
+                    src={`/api/v1/product/product-photo/${p._id}`}
+                    className="card-img-top"
+                    alt={p.name}
+                  />
+                  <div className="card-body">
+                    <h5>{p.name}</h5>
+                    <p> {p.description.substring(0, 30)}...</p>
+                    <h4> ${p.price}</h4>
+                    <button
+                      className=" button-1 ms-1"
+                      onClick={() => navigate(`/product/${p.slug}`)}
+                    >
+                      More Details
+                    </button>
+                    <button
+                      className=" button-2 ms-1"
+                      onClick={() => setCartItem(p)}
+                    >
+                      Add To Cart
+                    </button>
+                  </div>
+                </div>
+            ))}
+          </div>
         </div>
       </div>
-      <hr />
-      <div className="row container">
-        <h6>Similar Products</h6>
-        {relatedProducts.length < 1 && (
-          <p className="text-center">No Similar Products found</p>
-        )}
-        <div className="d-flex flex-wrap">
-          {relatedProducts?.map((p) => (
-            <div className="card m-2" style={{ width: "18rem" }}>
-              <img
-                src={`/api/v1/product/product-photo/${p?._id}`}
-                className="card-img-top"
-                alt={p.name}
-              />
-              <div className="card-body">
-                <h5 className="card-title">{p.name}</h5>
-                <p className="card-text">{p.description.substring(0, 30)}...</p>
-                <p className="card-text"> $ {p.price}</p>
-                <button
-                  className="btn btn-primary ms-1"
-                  onClick={() => navigate(`/product/${p.slug}`)}
-                >
-                  More Details
-                </button>
-                <button class="btn btn-secondary ms-1">ADD TO CART</button>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
+
+
     </Layout>
   );
 };
