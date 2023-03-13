5e88325d870a8d66731702508a427e8ac9ac6d7f
safawat hossain <sifakatali@gmail.com>
2023-03-14 00:07:10 +0600

    fix deploy error

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import path from 'path'
+import { fileURLToPath } from "url";
 
 
 
@@ -16,6 +17,8 @@ dotenv.config();
 //database config
 connectDB();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 
 //middelwares
